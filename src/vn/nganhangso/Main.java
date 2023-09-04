1dee75177df2a023ce964418c5aab0693f725567
HuyHoang <hoangit893@gmail.com>
2023-09-05 01:33:49 +0700

    OK

--- src/vn/nganhangso/Main.java
+++ src/vn/nganhangso/Main.java
@@ -1,14 +1,40 @@
 package vn.nganhangso;
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Math;
-
 import static java.lang.Integer.parseInt;
 
 public class Main {
-    public static int randomNumber(){
-        return (int)(Math.random() * 900 + 100);
+    public static int randomNumberByMinMax(int min, int max){
+        return (int) Math.floor(Math.random() *(max - min + 1) + min);
+    }
+    public static String randomString() {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+        int tempRnd ;
+        String generatedString = "";
+
+        for (int i = 0 ; i < 6; i++){
+            tempRnd = randomNumberByMinMax(0,AlphaNumericString.length() - 1);
+            generatedString += AlphaNumericString.substring(tempRnd,tempRnd + 1);
+        }
+        return generatedString;
+    }
+
+    public static boolean checkNumberByLength(String str, int length) {
+        if (str.length() != length) return false;
+
+        try {
+            for (int i = 0; i < str.length();i++){
+                Integer.parseInt(str.substring(i,i+1));
+            }
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 
     public static String processIdProvince(ArrayList<Province> listProvince ,
@@ -48,7 +74,7 @@ public class Main {
         final String VERSION = "@1.0.0";
         Scanner sc = new Scanner(System.in);
         ArrayList<Province> listProvince = new ArrayList<>();
-
+        // Add listProvince
         {
             listProvince.add(new Province("001", "Hà Nội"));
             listProvince.add(new Province("002", "Hà Giang"));
@@ -66,17 +92,69 @@ public class Main {
             listProvince.add(new Province("022", "Quảng Ninh"));
             listProvince.add(new Province("024", "Bắc Giang"));
             listProvince.add(new Province("025", "Phú Thọ"));
+            listProvince.add(new Province("026","Vĩnh Phúc"));
+            listProvince.add(new Province("027","Bắc Ninh"));
+            listProvince.add(new Province("030","Hải Dương"));
+            listProvince.add(new Province("031","Hải Phòng"));
+            listProvince.add(new Province("033","Hưng Yên"));
+            listProvince.add(new Province("034","Thái Bình"));
+            listProvince.add(new Province("035","Hà Nam"));
+            listProvince.add(new Province("036","Nam Định"));
+            listProvince.add(new Province("037","Ninh Bình"));
+            listProvince.add(new Province("038","Thanh Hóa"));
+            listProvince.add(new Province("040","Nghệ An"));
+            listProvince.add(new Province("042","Hà Tĩnh"));
+            listProvince.add(new Province("044","Quảng Bình"));
+            listProvince.add(new Province("045","Quảng Trị"));
+            listProvince.add(new Province("046","Thừa Thiên Huế"));
+            listProvince.add(new Province("048","Đà Nẵng"));
+            listProvince.add(new Province("049","Quảng Nam"));
+            listProvince.add(new Province("051","Quảng Ngãi"));
+            listProvince.add(new Province("052","Bình Định"));
+            listProvince.add(new Province("054","Phú Yên"));
+            listProvince.add(new Province("056","Khánh Hòa"));
+            listProvince.add(new Province("058","Ninh Thuận"));
+            listProvince.add(new Province("060","Bình Thuận"));
+            listProvince.add(new Province("062","Kon Tum"));
+            listProvince.add(new Province("064","Gia Lai"));
+            listProvince.add(new Province("066","Đắk Lắk"));
+            listProvince.add(new Province("067","Đắk Nông"));
+            listProvince.add(new Province("070","Bình Phước"));
+            listProvince.add(new Province("068","Lâm Đồng"));
+            listProvince.add(new Province("072","Tây Ninh"));
+            listProvince.add(new Province("074","Bình Dương"));
+            listProvince.add(new Province("75","Đồng Nai"));
+            listProvince.add(new Province("077","Bà Rịa - Vũng Tàu"));
+            listProvince.add(new Province("079","Hồ Chí Minh"));
+            listProvince.add(new Province("080","Long An"));
+            listProvince.add(new Province("082","Tiền Giang"));
+            listProvince.add(new Province("083","Bến Tre"));
+            listProvince.add(new Province("084","Trà Vinh"));
+            listProvince.add(new Province("086","Vĩnh Long"));
+            listProvince.add(new Province("087","Đồng Tháp"));
+            listProvince.add(new Province("089","An Giang"));
+            listProvince.add(new Province("091","Kiên Giang"));
+            listProvince.add(new Province("092","Cần Thơ"));
+            listProvince.add(new Province("093","Hậu Giang"));
+            listProvince.add(new Province("094","Sóc Trăng"));
+            listProvince.add(new Province("095","Bạc Liêu"));
+            listProvince.add(new Province("096","Cà Mau"));
         }
 
-//        final int lengthListProvince = listProvince.size() ;
-//        for (Province province : listProvince) {
-//            System.out.println(province.getId());
-//        }
-
-        int userSel , userConfirm ;
-        String idNumber ;
-
         final String SpaceLine = "+----------+---------------------------------------------+----------+";
+
+        System.out.println(SpaceLine);
+        System.out.println("|CAPTCHA MODE                                                       |");
+        System.out.println(SpaceLine);
+        System.out.println("|   1. EASY                                                         |");
+        System.out.println("|   0. HARD                                                         |");
+        System.out.println(SpaceLine);
+        System.out.print("Option : ");
+        int captchaMode = sc.nextInt();
+        int userSel ;
+
+
+
         System.out.println(SpaceLine);
         System.out.println("|NGAN HANG SO   |   " + AUTHOR + VERSION + "                                   |");
         System.out.println(SpaceLine);
@@ -89,49 +167,54 @@ public class Main {
             userSel = sc.nextInt() ;
         } while (userSel != 0 && userSel != 1) ;
 
-        System.out.println(userSel);
-
         if (userSel == 0){
             System.out.println("Exited");
         } else {
-            int tempRnd = randomNumber() ;
+            String tempRnd;
+
+            if (captchaMode == 1) {
+
+                tempRnd = String.valueOf(randomNumberByMinMax(100, 999));
+            } else {
+                tempRnd = randomString();
+            }
 
             //Yeu cau nguoi dung nhap ma xac nhan
             System.out.println("Nhap ma xac nhan : " + tempRnd);
+            String userConfirm;
+
+            System.out.println();
             do {
-                userConfirm = sc.nextInt();
-            } while (tempRnd != userConfirm);
+                userConfirm = sc.next();
+            } while (!tempRnd.equals(userConfirm));
 
+            String idNumber ;
+            do {
+                System.out.print("Nhap so CCCD : ");
+                idNumber = sc.next();
+            } while (!checkNumberByLength(idNumber,12));
 
-            try {
-                System.out.print("Nhap so CCCD : ");
-                 idNumber = sc.next();
-            } catch (Exception e) {
-                System.out.print("Nhap so CCCD : ");
-                 idNumber = sc.next();
-                System.out.println("Wrong input");
-            }
 
             final String idProvince = idNumber.substring(0,3);
             final String genderNumber = idNumber.substring(3,4);
             final String birthYear = idNumber.substring(4,6);
             final String rndNumberID = idNumber.substring(6,12);
-            System.out.println(idProvince + "   " + genderNumber + "   " + birthYear + "   " + rndNumberID );
-
-            System.out.println(processIdProvince(listProvince,idProvince) + "   " + processGender(genderNumber) + "     "+ processAge(genderNumber,birthYear));
 
             String func = " |    1. Kiểm tra nơi sinh \n |    2. Kiểm tra năm sinh, giới tính \n |    3. Kiểm tra số ngẫu nhiên \n |    0. Thoát \n Chọn chức năng : " ;
-            int selFunc = 5;
+
+            int selFunc;
             do {
                 try {
-                    System.out.println(func);
+                    System.out.print(func);
                     selFunc = sc.nextInt();
                 } catch (Exception e) {
-                     selFunc = sc.nextInt();
                     System.out.println("Wrong input");
+                    selFunc = sc.nextInt();
                 }
 
                 switch (selFunc) {
+                    case 0:
+                        break;
                     case 1 :
                         System.out.println("Nơi sinh :" + processIdProvince(listProvince,idProvince));
                         break;
@@ -142,7 +225,8 @@ public class Main {
                         System.out.println("Số ngẫu nhiên : " + rndNumberID);
                         break;
                     default:
-                        selFunc = 0;
+                        System.out.print("Vui lòng nhập lại : ");
+                        selFunc = sc.nextInt();
                 }
             } while (selFunc != 0) ;
         }
